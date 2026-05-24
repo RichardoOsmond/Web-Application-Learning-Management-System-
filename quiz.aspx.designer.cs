284a725cb2c9097b428a46462b49c907bc152e18
yumnuuu-ops <yumnuuu@gmail.com>
2026-05-24 15:27:26 +0800

    time limit js

--- quiz.aspx.designer.cs
+++ quiz.aspx.designer.cs
@@ -24,6 +24,24 @@ namespace Wapping_time
         protected global::System.Web.UI.WebControls.Label lblCourseQuizTitle;
 
         /// <summary>
+        /// hfTimeLimit control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hfTimeLimit;
+
+        /// <summary>
+        /// lblTimer control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTimer;
+
+        /// <summary>
         /// pnlMain control.
         /// </summary>
         /// <remarks>
